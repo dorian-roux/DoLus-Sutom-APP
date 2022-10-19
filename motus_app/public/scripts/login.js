27d70e8af78f624dacfa7b99cd658769d8973256
dorian-roux <mp.rouxdorian@gmail.com>
2022-10-20 01:13:47 +0200

    improvement of LoginReg

--- motus_app/public/scripts/login.js
+++ motus_app/public/scripts/login.js
@@ -1,54 +1,72 @@
 const btn_login = document.getElementById('submit_login');
+btn_login.addEventListener('click', makeLoginUser)
 
 const ipt_username = document.getElementById('clt_username')
 const ipt_password = document.getElementById('clt_password')
 
 
-btn_login.addEventListener("click", (event) => {   
 
+const txt_login_info = document.getElementById('login-error');
+
+
+function makeLoginUser(event) {
+
+    // Prevent the default action to be taken as it does not get explicitely handled.
     event.preventDefault()
 
     var username = ipt_username.value;
     var password = ipt_password.value;
 
-    if (username == ''){
-        window.alert('You must fill the USERNAME')
-    }else if(password == ''){
-        window.alert('You must fill the PASSWORD')
-    }else{
-        fetch('http://localhost:4200/loginUser/?username=' + username + '&password=' + password)
-        .then(response => {
-            if(response.ok){
-                return response.text();
-            }
-        }).then(text => {
-            if (text) {
-                if (text == "true"){
-                    fetch('http://localhost:4000/updateUserSession/?username=' + username)
-                    .then(response => {
-                        if (response.ok){
-                            return response.text();
-                        }
-                    })
-                    .then(() => {
-                        document.location = '/'
-                    })
-
-                }else if (text == "Wrong USERNAME / USERNAME not REGISTERED"){
-                    window.alert(text)
-
-                }else if (text== "Wrong PASSWORD / PASSWORD not REGISTERED"){
-                    window.alert(text)
-                }
-            }
-        })
+    if (username == '' && password == ''){
+        return getWarningInformation(txt_login_info, 'You must fill the LOGIN Identifiers')
     }
-})
 
+    if (username == ''){
+        return getWarningInformation(txt_login_info, 'You must fill the USERNAME')
 
-
-
-function register_user(){
+    }
+    if(password == ''){
+        return getWarningInformation(txt_login_info, 'You must fill the PASSWORD')
+    }
     
+    
+    fetch('http://localhost:4200/loginUser/?username=' + username + '&password=' + password)
+    .then(response => {
+        if(response.ok){
+            return response.text();
+        }
+    }).then(text => {
+        if (text) {
+            if (text == "true"){
+                fetch('http://localhost:4000/updateUserSession/?username=' + username)
+                .then(response => {
+                    if (response.ok){
+                        return response.text();
+                    }
+                })
+                .then(() => {
+                    document.location = '/'
+                })
 
+            }else{
+                return getWarningInformation(txt_login_info, text)                
+            }
+        }
+    })
+
+}
+
+
+
+
+// Function that display within an INPUTED area the INPUTED text as a warning/error
+function getWarningInformation(warning_element, warning_content){
+    warning_element.style.opacity = 1;
+    warning_element.style.transition = 'opacity linear 2s'; 
+    warning_element.innerHTML = '⚠️' + '  ' + warning_content
+    setTimeout(function() {
+        warning_element.style.opacity = 0;
+        warning_element.style.transition = 'opacity linear 2s';
+        warning_element.innerHTML = ''
+    }, 5000);
 }
--- motus_app/public/scripts/register.js
+++ motus_app/public/scripts/register.js
@@ -1,6 +1,7 @@
 const ipt_username = document.getElementById('nclt-username')
 const ipt_password = document.getElementById('nclt-password')
 const ipt_cpassword = document.getElementById('nclt-confirm-password')
+const txt_register_info = document.getElementById('register-error');
 
 const btn_register = document.getElementById('btn-register')
 btn_register.addEventListener("click", makeRegisterUser);
@@ -20,21 +21,26 @@ function makeRegisterUser(event){
 
 
 
-
     // --- Empty INPUT ---
     if (username == '' || password == '' || cpassword == ''){
         password = '';
         cpassword = '';
-        return window.alert('You must fill ALL the INPUTS')
+        return getWarningInformation(txt_register_info, 'You must fill the REGISTER Identifiers')
     }
 
     // --- Password and Password Confirmation differ ---
     if (password != cpassword ){
         password = '';
         cpassword = '';
-        return window.alert('You entered diferent PASSWORDS. Retry using the same.')
-
+        return getWarningInformation(txt_register_info, 'You entered different PASSWORDS. Retry using the same')
     }
+
+    // --- Check whether the Password Strengh
+    // cps = confirmPasswordStrength()
+    // if (cps != true){
+    //     return window.alert(cps)
+    // }
+
 
     fetch('http://localhost:4200/registerUserAccount/?username=' + username + '&password=' + password)
     .then(response => {
@@ -43,18 +49,56 @@ function makeRegisterUser(event){
         }
     }).then(text => {
         if (text) {
-            alert(text)
             if (text=="true"){
-                fetch('http://localhost:4100/registerUserScore/?username=' + username)
-                window.alert(`Your account ${username} has been created \n You will now be redirected to the Login`);
-                document.location = '/login';
+                fetch('http://localhost:4100/registerUserScore/?username=' + username);
+                swal('Account Successfully Created!', "You will now be redirected to the Login.", "success")
+                .then(() => {
+                    document.location = '/login';
+                })
             }else{
                 password = '';
                 cpassword = '';
-                return window.alert(text)
-
+                return getWarningInformation(txt_register_info, text)
             }
         }
     })
 
+}
+
+
+function confirmPasswordStrength(password){
+
+    // Regular Expressions 
+    var regex = new Array();
+    regex.push("[A-Z]"); //For Uppercase Alphabet
+    regex.push("[a-z]"); //For Lowercase Alphabet
+    regex.push("[0-9]"); //For Numeric Digits
+    regex.push("[$@$!%*#?&]"); //For Special Characters
+    var succeed = 0;
+    
+    // Verify each Regular Expressions
+    for (var i = 0; i < regex.length; i++) {
+        if((new RegExp (regex[i])).test(password)){
+            succeed++;
+        }
+    }
+
+    // Validate the PASSWORD
+    if(succeed == 4 && password.length > 8){
+        return true
+    }else{
+        return 'Your Password is not strong enough. Make sure to fill the requirements.'
+    }
+}
+
+// Function that display within an INPUTED area the INPUTED text as a warning/error
+function getWarningInformation(warning_element, warning_content){
+    warning_element.style.opacity = 1;
+    warning_element.style.transition = 'opacity linear 2s'; 
+    warning_element.innerHTML = '⚠️' + '  ' + warning_content
+    setTimeout(function() {
+        warning_element.style.opacity = 0;
+        warning_element.style.transition = 'opacity linear 2s';
+        warning_element.innerHTML = ''
+    }, 5000);
 }
